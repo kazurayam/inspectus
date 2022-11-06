645d11ede4f20e24a231fc5180f2dbef5210b938
kazurayam <kazuaki.urayama@gmail.com>
2022-11-06 15:16:07 +0900

    add the selenium subproject (=IDEA module)

--- selenium/src/main/java/com/kazurayam/inspectus/selenium/SeleniumStepListener.java
+++ selenium/src/main/java/com/kazurayam/inspectus/selenium/SeleniumStepListener.java
@@ -1,2 +1,21 @@
-package com.kazurayam.inspectus.selenium;public class SeleniumStepListener {
+package com.kazurayam.inspectus.selenium;
+
+import com.kazurayam.inspectus.core.StepListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SeleniumStepListener implements StepListener {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void stepStarted(String stepName) {
+        System.out.println(String.format("%s started", stepName));
+    }
+
+    @Override
+    public void stepFinished(String stepName) {
+        System.out.println(String.format("%s finished", stepName));
+    }
+
 }

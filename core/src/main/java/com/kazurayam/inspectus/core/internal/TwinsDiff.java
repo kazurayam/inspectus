928ee0213213f57d84cd04af6600baf27d9a7004
kazurayam <kazuaki.urayama@gmail.com>
2022-11-19 21:58:16 +0900

    use the materialstore-0.12.5-SNAPSHOT.jar

--- core/src/main/java/com/kazurayam/inspectus/core/internal/TwinsDiff.java
+++ core/src/main/java/com/kazurayam/inspectus/core/internal/TwinsDiff.java
@@ -132,8 +132,8 @@ public abstract class TwinsDiff extends AbstractDiffService {
                     MaterialProductGroup.builder(left, right)
                             .ignoreKeys("environment", "URL.host")
                             .ignoreKeys(parameters.getIgnoreMetadataKeys())
-                            .environmentLeft(environmentLeft.getValue())
-                            .environmentRight(environmentRight.getValue())
+                            .labelLeft(environmentLeft.getValue())
+                            .labelRight(environmentRight.getValue())
                             .build();
 
             // logger.info("parameters.getIgnoreMetadataKeys=" + parameters.getIgnoreMetadataKeys().toString());

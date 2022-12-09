4175fdd8134bdda31b52d7e5244d3e553b6dee5b
kazurayam <kazuaki.urayama@gmail.com>
2022-12-09 22:56:15 +0900

    cointed 0.7.1

---
+++ src/test/java/com/kazurayam/inspectus/materialize/discovery/ReadingXlsxTest.java
@@ -0,0 +1,55 @@
+package com.kazurayam.inspectus.materialize.discovery;
+
+import com.kazurayam.inspectus.materialize.TestHelper;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFTable;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReadingXlsxTest {
+
+    Path fixtureDir = TestHelper.getFixturesDirectory().resolve("com/kazurayam/inspectus/materialize/discovery/ReadingXlsxTest");
+
+    @Test
+    public void test_read_XSLS_file() throws IOException {
+        String sheetName = "Sheet1";
+        String tableName = "テーブル1";
+        Path xlsx = fixtureDir.resolve("names.xlsx");
+        assert Files.exists(xlsx): String.format("%s is not present", xlsx.toString());
+        FileInputStream fis = new FileInputStream(xlsx.toFile());
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheet(sheetName);
+        assert sheet != null;
+        List<XSSFTable> tables = sheet.getTables();
+        if (tables.size() == 0) {
+            throw new IOException("The sheet contains not Table");
+        }
+        XSSFTable table = null;
+        if (tables.size() == 1) {
+            table = tables.get(0);
+        } else {
+            List<XSSFTable> tablesWithTheName =
+                    tables.stream()
+                            .filter( t -> t.getName().equals(tableName))
+                            .collect(Collectors.toList());
+            if (tablesWithTheName.size() == 0) {
+                throw new IllegalStateException(
+                        String.format("no table named \"%s\" in the sheet named \"%s\"",
+                                tableName, sheetName));
+            }
+            if (tablesWithTheName.size() > 1) {
+                throw new IllegalStateException(
+                        String.format("there are 2 or more tables named \"%s\" in the sheet named \"%s\"",
+                                tableName, sheetName));
+            }
+            table = tablesWithTheName.get(0);
+        }
+    }
+}
